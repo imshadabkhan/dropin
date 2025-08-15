95f69f761a126c1edf6a9f3f8d1aa8b3b777d393
Shadab khan <117011709+imshadabkhan@users.noreply.github.com>
2025-08-16 01:34:23 +0500

    app color,onbaording_image added

---
+++ lib/core/constants/color_constants.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static var primaryColor = const Color(0xffFC8233);
+  static var blackColor = const Color(0xff000000);
+  static var onboardingGradientColor = const Color(0xffCEFFB8);
+  static var silverColor = const Color(0xffEAEFF2);
+  static var backgroundColor = const Color(0xfff7f7f7);
+  static var whiteColor = const Color(0xffFFFFFF);
+  static var redColor = const Color(0xffFD0D1B);
+  static var splashGradient = const Color(0xffCEFFB8);
+  static var blueBackgroundGradient1 = const Color(0xff08E3FF);
+  static var blueBackgroundGradient2 = const Color(0xff00709E);
+  static var orangeColorGradient1 = const Color(0xffC65308);
+  static var orangeColorGradient2 = const Color(0xffEE7628);
+  static var sentMessageBGColor = const Color(0xffD4F6FF);
+  static var receiveMessageBGColor = const Color(0xffFFECDF);
+  static var greyBgColor = const Color(0xffF7F7F7);
+  static var greenGradient1 = const Color(0xff26E138);
+  static var greenGradient2 = const Color(0xff1CA92A);
+  static var greenColor = const Color(0xff21C531);
+}
+
+
+
+
